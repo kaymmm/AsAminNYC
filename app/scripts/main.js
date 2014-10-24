265acf718e97e7b331fc3e461ad5bf31780a5249
Keith Miyake <kmiyake@gc.cuny.edu>
2014-10-24 01:34:13 -0400

    add sliding panels and begin filling out timeline

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,10 +6,15 @@
 'use strict';
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
+    $('a').bind('click', function(event) {
+      if ($.attr(this, 'href') === '#') {
+        //discard empty anchors
+        event.preventDefault();
+        return 0;
+      }
+      var nudge = $.attr(this, 'href') === '#about' ? 50 : 0;
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($.attr(this, 'href')).offset().top - $('.navbar').height() + nudge
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -17,7 +22,7 @@ $(function() {
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($('.navbar').offset().top > $('header').height() - 1) {
+    if ($('.navbar').offset().top >= $('header').height()) {
         $('.navbar-fixed-top').addClass('navbar-shrink');
     } else {
         $('.navbar-fixed-top').removeClass('navbar-shrink');
@@ -27,7 +32,8 @@ $(window).scroll(function() {
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+    target: '.navbar-fixed-top',
+    offset: $('.navbar').height()
 });
 
 // Closes the Responsive Menu on Menu Item Click
@@ -36,6 +42,43 @@ $('.navbar-collapse ul li a').click(function() {
 });
 
 // Initialize parallax effect
-$(function(){               
+$(function(){
     $('.parallax').simpleParallax();
+});
+
+// Initialize sliding panel
+$('.toggle-panel').click(function() {
+  var $container = $($(this).data('container'));
+  var $target = $($container.children($(this).data('target')));
+
+  $target.toggleClass('offcanvas');
+  $target.toggleClass('oncanvas');
+  if ($container.find('.oncanvas').length !== 0) {
+    $container.css('height',$container.children('.oncanvas').height());
+  } else {
+    $container.css('height',0);
+  }
+});
+$(function() {
+
+    var resizeTimer; // Set resizeTimer to empty so it resets on page load
+
+    function resizeFunction() {
+      $('.toggle-panel').each(function() {
+        var $container = $($(this).data('container'));
+        $container.css('height',$container.children('.oncanvas').height());
+      });
+    }
+
+    // On resize, run the function and reset the timeout
+    // 250 is the delay in milliseconds. Change as you see fit.
+    $(window).resize(function() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(resizeFunction, 500);
+    }).resize();
+});
+
+// Timeline toggles
+$('.toggle-timeline').click(function() {
+  $($(this).data('target')).slideToggle();
 });
